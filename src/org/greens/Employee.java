6643f25555b684180cd8d947e30518a15b2acb12
Sandhiya <gowthamia434@gmail.com>
2021-12-19 21:35:46 +0530

    nothing has done

---
+++ src/org/greens/Employee.java
@@ -0,0 +1,15 @@
+package org.greens;
+
+public class Employee {
+	
+	public void empId() {
+		System.out.println("Emp id:13");
+	}
+	public void empName() {
+		System.out.println("Emp Name:anu");
+	}
+	
+	
+	
+
+}
